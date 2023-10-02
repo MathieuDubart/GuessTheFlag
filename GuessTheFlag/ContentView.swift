72da2338ff74a58043d7287814ede6723f5c1f32
Mathieu Dubart <mathieu.dubart@icloud.com>
2023-10-02 11:16:04 +0200

    addding labels for accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -26,8 +26,24 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var playerScore = 0
+    @State private var selectedFlag = -1
     
     var body: some View {
         ZStack{
@@ -57,12 +73,12 @@ struct ContentView: View {
                             flagTapped(number)
                         } label: {
                             FlagImage(countries[number])
+                                .opacity(selectedFlag == -1 || selectedFlag == number ? 1 : 0.25)
+                                .scaleEffect(selectedFlag == -1 || selectedFlag == number ? 1 : 0.75)
+                                .animation(.easeInOut, value: selectedFlag)
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
-                        .alert(scoreTitle, isPresented: $showingScore) {
-                            Button("Continue", action: askQuestion)
-                        } message: {
-                            Text("Your score is \(playerScore)")
-                        }
+                        
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -76,14 +92,22 @@ struct ContentView: View {
                     .font(.title.bold())
                 Spacer()
             }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(playerScore)")
+            }
             .padding()
         }
     }
     
     func flagTapped(_ number:Int) {
+        selectedFlag = number
+        
         if number == correctAnswer {
             scoreTitle = "✅ Correct answer"
             playerScore += 1
+            
         } else {
             scoreTitle = "❌ Wrong answer"
         }
@@ -94,6 +118,7 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0..<3)
+        selectedFlag = -1
     }
 }
 
